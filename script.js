376fef187b6e993a27b064ab5b12afc2a00d1c0d
hydrghsn1435 <haydarghsn3@gmail.com>
2025-01-02 07:50:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Add any interactivity to your portfolio here
+document.querySelectorAll('video').forEach(video => {
+    video.addEventListener('click', () => {
+        console.log('Video clicked!');
+    });
+});
